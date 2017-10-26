9a889a768b8846b190ea21eae6f9f6cfc4532f47
saantiaguilera <aguilerasanti@hotmail.com>
2017-10-26 15:25:34 -0300

    Comment logic of the dummy backend

--- NodeServer/app.js
+++ NodeServer/app.js
@@ -7,15 +7,17 @@ app.use(morgan('combined'));
 app.use(bodyParser.json());
 
 app.get("/some/url", function(req, res) {
+  // Send the json file as response
   res.status(200).sendfile('some_url_get.json');
 });
 
 app.post("/some/url", function(req, res) { 
-  console.log(req.body);
+  // Mirror the body as response
   res.status(201).send(req.body);
 });
 
 app.get("/fail/url", function(req, res) {
+  // Send a fail response of Service Unavailable
   res.status(503).send(
     {
       message: "This is supposed to fail... So it failed"
@@ -23,4 +25,5 @@ app.get("/fail/url", function(req, res) {
   );
 });
 
+// Listen to port :3000
 app.listen(3000);
